103aaa9a64ddc52dc93b3663167dd9c29f9ba6a7
Magesh Kuppan <tkmagesh77@gmail.com>
2020-12-05 10:57:58 +0530

    app setup (folder structure)

--- my-app/src/index.js
+++ my-app/src/index.js
@@ -4,6 +4,21 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
+
+ReactDOM.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+  document.getElementById('root')
+);
+
+// If you want to start measuring performance in your app, pass a function
+// to log results (for example: reportWebVitals(console.log))
+// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+reportWebVitals();
+
+//ES6 Modules Examples
+
 /* 
 import * as calculator from './calc';
 console.log(calculator); 
@@ -28,18 +43,3 @@ console.log(calculator);
 
 //importing the default exported and regular exported entities together
 //import calculator, { add } from './calc';
-
-import add from './calc';
-console.log(add);
-
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
