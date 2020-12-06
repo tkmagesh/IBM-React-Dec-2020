231916859b5ba20c2e6b26f0156460ddfa780da0
Magesh Kuppan <tkmagesh77@gmail.com>
2020-12-06 12:51:39 +0530

    removing all closed bugs

--- my-app/src/bugTracker/actions/removeClosed.js
+++ my-app/src/bugTracker/actions/removeClosed.js
@@ -1,8 +1,12 @@
+import bugApi from '../services/bugApi';
 export function removeClosed() {
   return function(dispatch, getState){
     const bugs = getState().bugState;
     const bugsToRemove = bugs.filter(bug => bug.isClosed);
-    const action = { type: "BUG_REMOVE_MANY", payload: bugsToRemove };
-    dispatch(action);
+    bugsToRemove.forEach(async bugToRemove => {
+      await bugApi.remove(bugToRemove);
+      const action = { type: "BUG_REMOVE", payload: bugToRemove };
+      dispatch(action);
+    });
   }
 }
