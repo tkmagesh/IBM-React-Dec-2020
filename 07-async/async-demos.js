c94d24c744566320e5b1f04e990a35d4b35f50db
Magesh Kuppan <tkmagesh77@gmail.com>
2020-12-06 10:27:42 +0530

    Creating a promise based async fn

--- 07-async/async-demos.js
+++ 07-async/async-demos.js
@@ -14,20 +14,37 @@
 
     window['addSyncClient'] = addSyncClient;
 
-    function addAsync(x,y){
+    function addAsync(x,y, callback){
         console.log(`   [@Service] processing ${x} and ${y}`);
         setTimeout(() => {
-             const result = x + y;
+            const result = x + y;
             console.log(`   [@Service] returning result`);
-            return result;
+            callback(result);
         }, 5000);
     }
 
     function addAsyncClient(x,y){
         console.log(`[@Client] triggering the service`);
-        const result = addAsync(x,y);
-        console.log(`[@Client] result = ${result}`);
+        addAsync(x,y, function(result){
+            console.log(`[@Client] result = ${result}`);
+        });
     }
 
     window['addAsyncClient'] = addAsyncClient;
+
+    function addAsyncPromise(x,y){
+        console.log(`   [@Service] processing ${x} and ${y}`);
+        const p = new Promise(function(resolveFn, rejectFn){
+            setTimeout(() => {
+                const result = x + y;
+                console.log(`   [@Service] returning result`);
+                resolveFn(result);
+            }, 5000);
+        });
+        return p;
+    }
+
+    window['addAsyncPromise'] = addAsyncPromise;
 })();
+
+
